263fed2378d81f4d49d0c1869e5b5e87d0a139d3
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2024-03-30 23:44:49 -0500

    'first'

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Juan David Grijalba Osorio',
+            'email'=> 'juandavidgo1997@gmail.com',
+            'password'=> bcrypt('12341234'),
+        ]);
+
+        User::factory(99)->create();
+    }
+}
