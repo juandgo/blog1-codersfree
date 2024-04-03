b4fe6139c5d377db7cc840dc90ebc38b59586fd3
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2024-04-02 21:41:01 -0500

    posts list

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ use App\Models\Post;
 class PostController extends Controller
 {
     public function index(){
-        $posts = Post::where('status', 2)->get();
+        $posts = Post::where('status', 2)->latest('id')->paginate(8);
         return view('posts.index', compact('posts'));
     }
 }
